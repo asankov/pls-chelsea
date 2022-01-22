b9c88bbfdf39b2a0b3e0c1f4ab240abe3fd8274b
Anton Sankov <a.sankov96@gmail.com>
2022-01-22 21:53:34 +0200

    Remove boilerplate README.md

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,7 +3,7 @@ import { TeamRow } from "./TeamRow"
 export const Table = ({teams}) => {
     return (<div>
         {teams.map((team, i)=>(
-            <TeamRow position={i+1} name={team.fullName} homeResult={team.results?.home} awayResult={team.results?.away} key={i}></TeamRow>
+            <TeamRow position={i+1} name={team.fullName} points={team.points} homeResult={team.results?.home} awayResult={team.results?.away} key={i}></TeamRow>
         ))}
     </div>)
 }
--- src/components/TeamRow.jsx
+++ src/components/TeamRow.jsx
@@ -1,8 +1,9 @@
 import { Result } from "./Result";
 
-export const TeamRow = ({ position, name, homeResult, awayResult }) => (<div style={{ display: 'flex', paddingBottom: '5px' }}>
+export const TeamRow = ({ position, points, name, homeResult, awayResult }) => (<div style={{ display: 'flex', paddingBottom: '5px' }}>
     <div className="position" style={{ width: '50px' }}>{position}.</div>
     <div className="team-name" style={{ width: '500px' }}>{name}</div>
+    <div className="points" style={{ width: '50px' }}>{points}</div>
     {name != 'Chelsea' &&
         <>
             <Result result={homeResult}></Result>
